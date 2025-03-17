279c82f7f211532d54e20f707e39295342289a51
dansamoru <dansamoru@ya.ru>
2025-03-17 10:00:48 +0300

    Организация структуры

--- PhotoViewer.Console/Program.cs
+++ PhotoViewer.Console/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using PhotoViewer.Core;
+
+namespace PhotoViewer.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FolderNavigator navigator = new FolderNavigator();
+            Console.Write("Введите путь к папке с изображениями: ");
+            string path = Console.ReadLine();
+            navigator.ChangeFolder(path);
+
+            var images = navigator.GetImageFiles();
+            Console.WriteLine("Найденные изображения:");
+            foreach (var file in images)
+            {
+                Console.WriteLine(file);
+            }
+
+            Console.WriteLine("Нажмите любую клавишу для выхода...");
+            Console.ReadKey();
+        }
+    }
+}
--- PhotoViewer.Core/FolderNavigator.cs
+++ PhotoViewer.Core/FolderNavigator.cs
@@ -1,7 +1,35 @@
 ﻿namespace PhotoViewer.Core
 {
-    public class Class1
+    public class FolderNavigator
     {
+        // Свойство для текущего пути
+        public string CurrentPath { get; set; }
 
+        // Делегат и событие для уведомления об изменении папки
+        public delegate void FolderChangedEventHandler(string newPath);
+        public event FolderChangedEventHandler FolderChanged;
+
+        // Метод для получения списка файлов (например, изображений)
+        public List<string> GetImageFiles()
+        {
+            if (string.IsNullOrEmpty(CurrentPath) || !Directory.Exists(CurrentPath))
+                return new List<string>();
+
+            // Фильтрация по расширениям файлов
+            var supportedExtensions = new[] { ".jpg", ".jpeg", ".png", ".bmp", ".gif" };
+            return Directory.GetFiles(CurrentPath)
+                            .Where(file => supportedExtensions.Contains(Path.GetExtension(file).ToLower()))
+                            .ToList();
+        }
+
+        // Метод для смены папки
+        public void ChangeFolder(string newPath)
+        {
+            if (Directory.Exists(newPath))
+            {
+                CurrentPath = newPath;
+                FolderChanged?.Invoke(newPath);
+            }
+        }
     }
 }
